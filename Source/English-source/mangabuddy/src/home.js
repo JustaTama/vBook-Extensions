84468d5e6e38924c43eff0470779fbf24892578b
Minh Tâm <65128440+JustaTama@users.noreply.github.com>
2022-11-19 13:12:20 +0700

    fix

--- Source/English-source/mangabuddy/src/home.js
+++ Source/English-source/mangabuddy/src/home.js
@@ -1,7 +1,10 @@
 function execute() {
     return Response.success([
-        { title: "Lastest", input: "/latest", script: "gen.js" },
-        { title: "Popular", input: "/popular", script: "gen.js" },
-        { title: "Top Today", input: "/top/day", script: "gen.js" },
+        { title: "Lastest Updates", input: "/latest", script: "gen.js" },
+        { title: "Popular Manga", input: "/popular", script: "gen.js" },
+        { title: "Top of the day", input: "/top/day", script: "gen.js" },
+        { title: "Top of the week", input: "/top/week", script: "gen.js" },
+        { title: "Top of the month", input: "/top/month", script: "gen.js" },
+        { title: "Completed Manga", input: "/status/Completed", script: "gen.js" },
     ]);
 }
